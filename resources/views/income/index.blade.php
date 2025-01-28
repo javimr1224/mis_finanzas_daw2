494e4e77176d03f006f9bc8f46d2fc8e7933764d
javi <javimr1224@gmail.com>
2025-01-28 13:25:53 +0100

    Tarea3

--- resources/views/income/index.blade.php
+++ resources/views/income/index.blade.php
@@ -1,5 +1,5 @@
 
 <x-layouts.index :title="$title">
   <x-table :tableData="$tableData" class="" />
-  <x-button href="https://minecraft.com/" class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">Mi botón</x-button> 
+  <x-button  class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">Mi botón<x-table>
 </x-layouts.index>
---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Models\Income;
+
+Route::get('/incomes', function (Request $request) {
+    return Income::all();
+    // return $request->user();
+}); //->middleware('auth:sanctum');
